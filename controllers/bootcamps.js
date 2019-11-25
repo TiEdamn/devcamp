d8d40b70cc77d7fb54cb18e98c4a3ae7dc15aba5
Igor Tikhanovich <tikhanovich.89@gmail.com>
2019-11-25 16:42:14 +0300

    error response and error handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResnpose');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -27,9 +28,12 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            });
+            return next(
+                new ErrorResponse(
+                    `Bootcamp not found with id of ${req.params.id}`,
+                    404
+                )
+            );
         }
 
         res.status(200).json({
@@ -37,9 +41,12 @@ exports.getBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (error) {
-        res.status(400).json({
-            success: false
-        });
+        next(
+            new ErrorResponse(
+                `Bootcamp not found with id of ${req.params.id}`,
+                404
+            )
+        );
     }
 };
 
@@ -53,7 +60,7 @@ exports.createBootcamp = async (req, res, next) => {
             success: true,
             data: bootcamp
         });
-    } catch (err) {
+    } catch (error) {
         res.status(400).json({
             success: false
         });
